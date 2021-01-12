a3174d7fe221939268460b5718b26e89e4362c9f
Gabriel Loza Lucero <sergio.loza.l@ucb.edu.bo>
2021-01-12 15:55:47 -0400

    main page components added

--- frontPC/src/app/app.module.ts
+++ frontPC/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main-page/header/header.component';
+import { SignInComponent } from './main-page/sign-in/sign-in.component';
+import { SignUpComponent } from './main-page/sign-up/sign-up.component';
+import { HomeComponent } from './main-page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
