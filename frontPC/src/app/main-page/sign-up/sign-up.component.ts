059f58bb9f511334a068601d78a186ea439335e4
GaboLozaLucero <69486606+GaboLozaLucero@users.noreply.github.com>
2021-01-14 05:16:14 -0400

    Update sign-up.component.ts
    register user working

--- frontPC/src/app/main-page/sign-up/sign-up.component.ts
+++ frontPC/src/app/main-page/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Observable } from 'rxjs';
+import {Router} from '@angular/router';
+import {ServiceService} from './serviceSignUp/service.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: any;
+  titleAlert = 'This field is required';
+  post: any = '';
+
+  constructor(private formBuilder: FormBuilder, private serviceService: ServiceService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
-
+  // tslint:disable-next-line:typedef
+  createForm(){
+    this.formGroup = this.formBuilder.group({
+      cityId: [null, [Validators.required]],
+      companyId: [null, [Validators.required]],
+      name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(97)]],
+      lastname: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(97)]],
+      phone: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(10)]],
+      birthday: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      email: [null, [Validators.required, Validators.minLength(13), Validators.maxLength(60), Validators.email]],
+      username: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
+      password: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(80)]]
+    });
+  }
+  // tslint:disable-next-line:typedef
+  onSubmit(post: any){
+    // tslint:disable-next-line:max-line-length
+    this.serviceService.createUser({
+      'cityId': post.cityId,
+      'companyId': 2,
+      'name': post.name,
+      'lastname': post.lastname,
+      'phone': post.phone,
+      'birthday': post.birthday,
+      'email': post.email,
+      'username': post.username,
+      'password': post.password}).subscribe(data => {alert('User registered correctly');});
+  }
 }
+
